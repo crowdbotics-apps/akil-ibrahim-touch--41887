f3914f99f1731523951324e084bc0d4f7439c19a
Crowdbotics <team@crowdbotics.com>
2023-07-03 23:17:45 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "akil-ibrahim-touch--41887",
+  url: "https://akil-ibrahim-touch--41887.botics.co",
+  api: "https://akil-ibrahim-touch--41887.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
